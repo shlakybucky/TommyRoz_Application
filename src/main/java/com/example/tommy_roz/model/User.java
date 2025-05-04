283f82d19e3fa165b98cb994793e7d5bcd4f44d8
Bottle Alba <bottleal@Bottles-MacBook-Pro.local>
2025-05-04 20:03:30 +0300

    edited user class, user repository, user controller, authorization

--- src/main/java/com/example/tommy_roz/model/User.java
+++ src/main/java/com/example/tommy_roz/model/User.java
@@ -1,62 +1,102 @@
-//package model;
-//
-////import jakarta.persistence.Entity;
-//import jakarta.persistence.*;
-////import jakarta.persistence.GeneratedValue;
-////import jakarta.persistence.GenerationType;
-////import jakarta.persistence.Id;
-//
-//@Entity
-//@Table(name = "users")
-//public class User {
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
-//    private Long id;
-//    private String fullName;
-//    private String username;
-//    private String phone;
-//    private String email;
-//    private String password;
-//    private String role; //simple user or premium user
-//
-//    //constructors
-//    public User(){
-//
-//    }
-//    public User(String fullName, String password, String username, String phone, String email){
+package com.example.tommy_roz.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+@Table(name = "users")
+@AllArgsConstructor
+@Data
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String fullName;
+    private Integer age;
+    private String login;
+    private String phone;
+    private String email;
+    private String password;
+    private String role; //simple user or premium user
+
+    //constructors
+    public User() {
+    }
+
+//    public User(String fullName, String password, String login, String phone, String email) {
 //        this.fullName = fullName;
 //        this.password = password;
-//        this.username = username;
+//        this.login = login;
 //        this.phone = phone;
 //        this.email = email;
 //        this.role = "ROLE_USER";
 //    }
-//
-//    //getters and setters
-//    public Long getId(){return id;}
-//    public void setId(Long id){this.id = id;}
-//
-//    public String getFullName(){return fullName;}
-//    public void setFullName(String fullName){this.fullName = fullName;}
-//
-//    public void setUsername(String fullName){this.username = username;}
-//    public String getUsername(){return username;}
-//
-//    public void setPhone(String phone){this.phone = phone;}
-//    public String getPhone(){return phone;}
-//
-//    public String getEmail(){return email;}
-//    public void setEmail(String email){this.email = email;}
-//
-//    public String getPassword(){ return password;}
-//    public void setPassword(String password){this.password = password;}
-//
-//    public String getRole(){return role;}
-//    public void setRole(String role){
-//        //if(role = null || role.isEmpty()){
-//           // this.role = "ROLE_USER";
-//        //}else{
-//            this.role = role;
-//       // }
+
+    //getters and setters
+//    public Long getId() {
+//        return id;
 //    }
-//}
+//
+//    public void setId(Long id) {
+//        this.id = id;
+//    }
+//
+//    public String getFullName() {
+//        return fullName;
+//    }
+//
+//    public void setFullName(String fullName) {
+//        this.fullName = fullName;
+//    }
+//
+//    public void setlogin(String fullName) {
+//        this.login = login;
+//    }
+//
+//    public String getlogin() {
+//        return login;
+//    }
+//
+//    public void setPhone(String phone) {
+//        this.phone = phone;
+//    }
+//
+//    public String getPhone() {
+//        return phone;
+//    }
+//
+//    public String getEmail() {
+//        return email;
+//    }
+//
+//    public void setEmail(String email) {
+//        this.email = email;
+//    }
+//
+//    public String getPassword() {
+//        return password;
+//    }
+//
+//    public void setPassword(String password) {
+//        this.password = password;
+//    }
+//
+//    public String getRole() {
+//        return role;
+//    }
+//
+//    public void setRole(String role) {
+//        if (role == null || role.isEmpty()) {
+//            this.role = "ROLE_USER";
+//        } else {
+//            this.role = role;
+//        }
+//    }
+
+}
+
--- src/main/java/com/example/tommy_roz/repository/UserRepo.java
+++ src/main/java/com/example/tommy_roz/repository/UserRepo.java
@@ -1,9 +1,9 @@
-//package repository;
-//
-//import org.springframework.data.jpa.repository.JpaRepository;
-//import java.util.Optional;
-//import model.User;
-//
-//public interface UserRepo extends JpaRepository<User, Long> {
-//    Optional<User> findByUserName(String username);
-//}
+package com.example.tommy_roz.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+import com.example.tommy_roz.model.User;
+
+public interface UserRepo extends JpaRepository<User, Long> {
+    Optional<User> findByLogin(String login);
+}
