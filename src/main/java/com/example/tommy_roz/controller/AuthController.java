47e5b53547fd4670f5e517e487b13e5f533146be
Bottle Alba <bottleal@Bottles-MacBook-Pro.local>
2025-05-07 20:20:02 +0300

    я уже запуталась, надо подключить jwt, придется переписывать

--- src/main/java/com/example/tommy_roz/controller/AuthController.java
+++ src/main/java/com/example/tommy_roz/controller/AuthController.java
@@ -3,11 +3,15 @@ package com.example.tommy_roz.controller;
 import com.example.tommy_roz.service.UserService;
 import com.example.tommy_roz.dto.SignUpRequest;
 
+
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
+//import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+
 
 
 @Controller
@@ -16,19 +20,20 @@ public class AuthController {
     @Autowired
     private UserService userService;
 
-    @GetMapping("/auth/login")
+    @GetMapping("/login")
     public String showLoginForm(){
         return "login_form";
     }
 
-    @PostMapping("/auth/login")
+    @PostMapping("/login")
     public String login(){
         //add login method
         return "Successfully signed in";
     }
 
-    @GetMapping("/auth/signup")
-    public String showSignUpForm(){
+    @GetMapping("/signup")
+    public String showSignUpForm(Model model){
+        model.addAttribute("signUpRequest", new SignUpRequest());
         return "sign_up_form";
     }
 
@@ -41,10 +46,23 @@ public class AuthController {
 //        userService.signUp(login, password, fullName, phone, email);
 //        return "redirect:/auth/login";
 //    }
-    @PostMapping("/auth/signup")
-    public ResponseEntity<String> register(@RequestBody @Valid SignUpRequest request) {
-        userService.signUp(request);
-        return ResponseEntity.ok("User is registered successfully");
+    @PostMapping("/signup")
+    public /*ResponseEntity<String>*/ String register(/*@RequestBody*/ @Valid @ModelAttribute("signUpRequest") SignUpRequest request,
+                                                            BindingResult result,
+                                                            Model model) {
+        if(result.hasErrors()){
+            return "sign_up_form";
+        }
+        try{
+           userService.signUp(request);
+           return "redirect:/auth/login";
+        }catch(RuntimeException e){
+            model.addAttribute("errorMessage", e.getMessage());
+            return "sign_up_form";
+        }
+
+        //userService.signUp(request);
+//        return ResponseEntity.ok("User is registered successfully");
     }
 
 }
